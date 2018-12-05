3ceb6f83d6b906b0d65ffb971d26a36f4d6b73a6
Ted Strzalkowski <tedski@gmail.com>
2018-12-04 18:35:00 -0800

    Day 3, part 1

---
+++ 2018/day03/solution.py
@@ -0,0 +1,56 @@
+import os
+from collections import Counter
+from typing import List
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+input_file = os.path.join(curdir, 'input.txt')
+
+with open(input_file, 'r') as fp:
+    puzzle_input = fp.read().splitlines()
+
+def get_points(claim: str) -> List[tuple]:
+    points = []
+    claimid, _, raw_addr, raw_size = claim.split()
+    addr = tuple(map(int, raw_addr.rstrip(':').split(',')))
+    size = tuple(map(int, raw_size.split('x')))
+
+    for y in range(1, size[1] + 1):
+        for x in range(1, size[0] + 1):
+            points.append((addr[0] + x, addr[1] + y))
+
+    return points
+
+
+def overlap_count(claims: list) -> int:
+    used_points = set()
+    overlap_points = set()
+    for claim in claims:
+
+        points = get_points(claim)
+
+        for point in points:
+            if point in used_points:
+                overlap_points.add(point)
+            else:
+                used_points.add(point)
+        
+    return len(overlap_points)
+
+
+def test_get_points():
+    expected = Counter([(4, 3), (5, 3), (6, 3), (7, 3), (8, 3),
+                        (4, 4), (5, 4), (6, 4), (7, 4), (8, 4),
+                        (4, 5), (5, 5), (6, 5), (7, 5), (8, 5),
+                        (4, 6), (5, 6), (6, 6), (7, 6), (8, 6)])
+    assert Counter(get_points('#123 @ 3,2: 5x4')) == expected
+
+
+def test_overlap_points():
+    claims = ['#1 @ 1,3: 4x4',
+              '#2 @ 3,1: 4x4',
+              '#3 @ 5,5: 2x2']
+    assert overlap_count(claims) == 4
+
+    
+if __name__ == '__main__':
+    print(f'square inches of fabric within two or more claims: {overlap_count(puzzle_input)}')
