955565676bdbf7a89b25e527c3767d6078822040
Ted Strzalkowski <tedski@gmail.com>
2018-12-07 16:36:15 -0800

    Day 5, Part 2

--- 2018/day05/day05.py
+++ 2018/day05/day05.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import os
 from string import ascii_lowercase
 
@@ -18,12 +19,30 @@ def reactor(polymer: str) -> str:
     return polymer
 
 
+def improvement_recommender(polymer: str) -> str:
+    experiment_data = Counter()
+    units = set(polymer.lower())
+    for letter in units:
+        reactants = polymer.replace(letter.upper(), '').replace(letter, '')
+        product = reactor(reactants)
+        experiment_data.update({letter: len(product)})
+    best_improvement = experiment_data.most_common()[:-2:-1][0]
+    return best_improvement
+
+
 def test_reactor():
     polymer = 'dabAcCaCBAcCcaDA'
     assert reactor(polymer) == 'dabCBAcaDA'
 
 
+def test_improvement():
+    polymer = 'dabAcCaCBAcCcaDA'
+    assert improvement_recommender(polymer) == 'c'
+
+
 if __name__ == '__main__':
     stable_polymer = reactor(puzzle_input)
     unit_count = len(stable_polymer)
+    unit, length = improvement_recommender(puzzle_input)
     print(f'{unit_count} units remain after fully reacting the polymer')
+    print(f'Removing {unit} yields the shortest polymer of length {length}.')
